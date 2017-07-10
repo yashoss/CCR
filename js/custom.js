4026ca10befffd20626281544f457ca1b041badf
yasin <yashoss@gmail.com>
2017-07-09 17:55:46 -0700

    wrong api url

--- js/custom.js
+++ js/custom.js
@@ -36,7 +36,7 @@ function calculateCenter() {
 function loadGoogleMap(){
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCbCJSxtObL-0675mcR4OuOzK-BKCHsfxA' + 'callback=initialize';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCbCJSxtObL-0675mcR4OuOzK-BKCHsfxA&' + 'callback=initialize';
     document.body.appendChild(script);
 }
 
